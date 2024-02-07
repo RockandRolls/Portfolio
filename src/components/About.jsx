9524687440352c88a6f9ce6a4427c42ade25d387
RockandRolls <livelydev010@gmail.com>
2024-02-07 13:17:42 +0100

    initial frame

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+const About = () => {
+    return (
+      <>
+        <div>
+            <img src="" alt="my pic" />
+            <img src="" alt="linkedin link icon " /> 
+            <img src="" alt="git hub link icon" /> 
+            <img src="" alt="email link icon" /> 
+        </div>
+        <div>
+          <p> Hi,
+
+          </p>       
+        </div>
+      </>
+      
+    )
+  }
+  
+  export default About 
