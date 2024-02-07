a1b3af12de055467e92d63da8a1cc17f9fa87589
RockandRolls <livelydev010@gmail.com>
2024-02-07 14:32:07 +0100

    addes images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ const About = () => {
     return (
       <>
         <div>
-            <img src="" alt="my pic" />
-            <img src="" alt="linkedin link icon " /> 
-            <img src="" alt="git hub link icon" /> 
-            <img src="" alt="email link icon" /> 
+            <img src="./RIphoto.jpg" alt="my pic" />
+            <img className="h-7 w-8" viewBox="0 0 24 24" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvn02Fzoa-K9jHv2kJrTnjkurQaiQydba_7g&usqp=CAU" alt="linkedin link icon " /> 
+            <img className="h-7 w-8" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="git hub link icon" /> 
+            <img className="h-7 w-8"  src="https://www.svgrepo.com/show/295341/envelope-email.svg" alt="email link icon" /> 
         </div>
         <div>
-          <p> Hi,
+          <p className="text-3xl "> Hi,
 
           </p>       
         </div>
